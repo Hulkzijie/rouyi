c2f45263d7bf11f6193869473fcd1bb228d4bd83
王子杰 <wangzijie_xm.cicdi@chinaccs.cn>
2025-03-05 10:02:58 +0800

    initdata

---
+++ src/pages/work/work.vue
@@ -0,0 +1,46 @@
+<template>
+  <view>
+    <view class="work">
+      <view class="work__title">
+        <view class="work__title-text">工作</view>
+      </view>
+      <view class="work__list">
+        <view class="work__list-item" v-for="(item, index) in list" :key="index">
+          <view class="work__list-item-title">
+            {{ item.title }}
+          </view>
+          <view class="work__list-item-content">
+            {{ item.content }}
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+defineOptions({
+  name: 'Work',
+})
+const list = ref([
+  {
+    title: '前端开发',
+    content: '前端开发',
+  },
+  {
+    title: '后端开发',
+    content: '后端开发',
+  },
+  {
+    title: '小程序开发',
+    content: '小程序开发',
+  },
+])
+// 测试 uni API 自动引入
+onLoad(() => {
+  uni.showToast({
+    title: '加载中',
+    icon: 'loading',
+  })
+})
+</script>
