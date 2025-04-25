c38cf8af9744694f1f37976c46299d0e260b18b7
王子杰 <wangzijie_xm.cicdi@chinaccs.cn>
2025-04-25 10:12:42 +0800

    init

--- src/components/AppTest.vue
+++
@@ -1,7 +0,0 @@
-<!-- 本文件会自动导入 -->
-<template>
-  <view>
-    <view>components 里面的 AppTest.vue 组件</view>
-    <slot />
-  </view>
-</template>
--- src/components/app-test-dir/AppTest2.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <view>
-    <view>components 里面的 app-test-dir/AppTest.vue 组件</view>
-    <slot />
-  </view>
-</template>
